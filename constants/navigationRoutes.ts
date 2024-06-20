b022f148549db336b09a491a69ddc2c95ecd94ac
Diego Cardenas <diego.cardenasaleg@gmail.com>
2024-06-19 19:50:48 -0500

    added navigation routes to handle as parameter with navigate call function

---
+++ constants/navigationRoutes.ts
@@ -0,0 +1,9 @@
+import {
+  FinancialProductDetail,
+  FinancialProductForm,
+  HomeRoute
+} from "@/navigation";
+
+export const homeRoute = "Home" as HomeRoute;
+export const financialProductDetailRoute = "FinancialProductDetail" as FinancialProductDetail;
+export const financialProductFormRoute = "FinancialProductForm" as FinancialProductForm;
