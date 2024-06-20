ff0d59cb38431f81a7b5ec0596a71174e6dcb492
Diego Cardenas <diego.cardenasaleg@gmail.com>
2024-06-19 19:48:03 -0500

    added nerw constannts to handle in views

---
+++ constants/global.ts
@@ -0,0 +1,13 @@
+export const ButtonColors = {
+  primary: "#FFDD00",
+  secondary: "#E9ECF3",
+  danger: "#D50707"
+};
+
+export const initialErrorsState = {
+  id: null,
+  name: null,
+  description: null,
+  logo: null,
+  releaseDate: null
+};
